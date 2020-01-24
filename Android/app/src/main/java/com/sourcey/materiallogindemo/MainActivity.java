da2403eb70d0d004b87ad59bcf70aa6938a5f9c0
Dhruv <dhruvchauhan100@gmail.com>
2020-01-24 09:32:10 +0545

    new files added android and webhost

---
+++ Android/app/src/main/java/com/sourcey/materiallogindemo/MainActivity.java
@@ -0,0 +1,89 @@
+package com.sourcey.materiallogindemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
+
+    Spinner spinner,spinner1,spinner2;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+        spinner = findViewById(R.id.spinner);
+        spinner1 =  findViewById(R.id.spinner1);
+        spinner2 = findViewById(R.id.spinner2);
+
+        ArrayAdapter<CharSequence> adapter =
+                ArrayAdapter.createFromResource(this,R.array.subjects,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+        spinner1.setAdapter(adapter);
+        spinner1.setOnItemSelectedListener(this);
+        spinner2.setAdapter(adapter);
+        spinner2.setOnItemSelectedListener(this);
+
+        button.setOnClickListener(this);
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Main2Activity();
+    }
+    public void Main2Activity() {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+}
---
+++ WebHost/connect.php
@@ -0,0 +1,5 @@
+<?php
+  define ( 'DB_HOST','localhost' );
+  define ( 'DB_USER','id12338909_electiveamrita' );
+  define ( 'DB_PASSWORD','elective' );
+?>
