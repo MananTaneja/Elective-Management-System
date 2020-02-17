e97c6783d2c0f279779ca91fedd2a297a0d42d3a
MananTaneja <manantaneja007@gmail.com>
2020-02-17 12:27:57 +0530

    cleared repo

--- WebHost/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-  require_once "constants.php";
-
-  $con = new mysqli(DB_HOST, DB_USER, DB_PASSWORD);
-  
-?>
--- WebHost/constants.php
+++
@@ -1,6 +0,0 @@
-<?php
-  define ( 'DB_HOST','localhost' );
-  define ( 'DB_USER','id12338909_electiveamrita' );
-  define ( 'DB_PASSWORD','elective' );
-  define ( 'DB_NAME', 'id12338909_electivemanagement');
-?>
